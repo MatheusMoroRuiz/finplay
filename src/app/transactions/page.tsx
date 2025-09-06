27853c19bab65f721926c4cd28bb4a3d82ad63ab
Matheus Moro <matheusfigter@gmail.com>
2025-09-06 10:32:22 -0300

    feat: ensure transactions and subscriptions pages are protected

--- src/app/transactions/page.tsx
+++ src/app/transactions/page.tsx
@@ -3,10 +3,24 @@ import { DataTable } from "../_components/ui/data-table";
 import { transactionColumns } from "./_columns";
 import AddTransactionButton from "../_components/add-transaction-button";
 import Navbar from "../_components/navbar";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
 const TransactionsPage = async () => {
+  // Verifica se o usuário está autenticado usando Clerk e redireciona para a página de login se não estiver
+  const { userId } = await auth();
+  if (!userId) {
+    redirect("/login");
+  }
   // O findMany serve para buscar todas as transações no banco na tabela Transaction
-  const transactions = await db.transaction.findMany({});
+  const transactions = await db.transaction.findMany({
+    where: {
+      userId: userId, // Filtra as transações pelo ID do usuário autenticado
+    },
+    orderBy: {
+      date: "desc", // Ordena as transações pela data em ordem decrescente
+    },
+  });
   return (
     <>
       <Navbar />
