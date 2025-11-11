b99e625a76bc48422198a745cb2b13b0a497b448
Matheus Moro <matheusfigter@gmail.com>
2025-11-10 22:21:58 -0300

    feat: add last transactions components

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -10,6 +10,7 @@ import { isMatch } from "date-fns";
 import TransactionsPieChart from "./_components/transactions-pie-chart";
 import { getDashboard } from "../_data/get-dashboard";
 import ExpensesPerCategory from "./_components/expenses-per-category";
+import LastTransactions from "./_components/last-transactions";
 
 interface HomeProps {
   searchParams: {
@@ -42,38 +43,45 @@ const Home = async ({ searchParams: { month } }: HomeProps) => {
           <h1 className="text-2xl font-bold">Dashboard</h1>
           <TimeSelect />
         </div>
-        {/* Componente que exibe os cards de resumo financeiro */}
-        <SummaryCards
-          month={month}
-          investmentsTotal={
-            dashboard.investmentsTotal ? Number(dashboard.investmentsTotal) : 0
-          }
-          expensesTotal={
-            dashboard.expensesTotal ? Number(dashboard.expensesTotal) : 0
-          }
-          depositsTotal={
-            dashboard.depositsTotal ? Number(dashboard.depositsTotal) : 0
-          }
-          balance={dashboard.balance}
-        />
+        <div className="grid grid-cols-[2fr,1fr] gap-6">
+          <div className="flex flex-col gap-6">
+            {/* Componente que exibe os cards de resumo financeiro */}
+            <SummaryCards
+              month={month}
+              investmentsTotal={
+                dashboard.investmentsTotal
+                  ? Number(dashboard.investmentsTotal)
+                  : 0
+              }
+              expensesTotal={
+                dashboard.expensesTotal ? Number(dashboard.expensesTotal) : 0
+              }
+              depositsTotal={
+                dashboard.depositsTotal ? Number(dashboard.depositsTotal) : 0
+              }
+              balance={dashboard.balance}
+            />
 
-        <div className="grid grid-cols-3 grid-rows-1 gap-6">
-          <TransactionsPieChart
-            investmentsTotal={
-              dashboard.investmentsTotal
-                ? Number(dashboard.investmentsTotal)
-                : 0
-            }
-            expensesTotal={
-              dashboard.expensesTotal ? Number(dashboard.expensesTotal) : 0
-            }
-            depositsTotal={
-              dashboard.depositsTotal ? Number(dashboard.depositsTotal) : 0
-            }
-          />
-          <ExpensesPerCategory
-            expensesPerCategory={dashboard.totalExpensePerCategory}
-          />
+            <div className="grid grid-cols-3 grid-rows-1 gap-6">
+              <TransactionsPieChart
+                investmentsTotal={
+                  dashboard.investmentsTotal
+                    ? Number(dashboard.investmentsTotal)
+                    : 0
+                }
+                expensesTotal={
+                  dashboard.expensesTotal ? Number(dashboard.expensesTotal) : 0
+                }
+                depositsTotal={
+                  dashboard.depositsTotal ? Number(dashboard.depositsTotal) : 0
+                }
+              />
+              <ExpensesPerCategory
+                expensesPerCategory={dashboard.totalExpensePerCategory}
+              />
+            </div>
+          </div>
+          <LastTransactions lastTransactions={dashboard.lastTransactions} />
         </div>
       </div>
     </>
--- src/app/_constants/transactions.ts
+++ src/app/_constants/transactions.ts
@@ -14,6 +14,16 @@ export const TRANSACTION_CATEGORY_LABELS = {
   OTHER: "Outros",
 };
 
+export const TRANSACTION_PAYMENT_METHOD_ICONS = {
+  [TransactionPaymentMethod.CREDIT_CARD]: "credit-card.svg",
+  [TransactionPaymentMethod.DEBIT_CARD]: "debit-card.svg",
+  [TransactionPaymentMethod.BANK_TRANSFER]: "bank-transfer.svg",
+  [TransactionPaymentMethod.BANK_SLIP]: "bank-slip.svg",
+  [TransactionPaymentMethod.CASH]: "money.svg",
+  [TransactionPaymentMethod.PIX]: "pix.svg",
+  [TransactionPaymentMethod.OTHER]: "other.svg",
+};
+
 export const TRANSACTION_PAYMENT_METHOD_LABELS = {
   CREDIT_CARD: "Cartão de Crédito",
   DEBIT_CARD: "Cartão de Débito",
